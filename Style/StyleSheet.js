df8f6d8309f76ef74ab49d92cd23b181b5839f30
Milena <milena.basso444@gmail.com>
2024-03-22 12:40:17 -0300

    create style

---
+++ Style/StyleSheet.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 50,
+  },
+
+  input: {
+    width: 300,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "black",
+  },
+});
