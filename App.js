a54bda5ea3b15f55a19a6d5abd8dc189581ef63d
Milena <milena.basso444@gmail.com>
2024-03-22 13:42:47 -0300

    modal created

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { styles } from "./Style/StyleSheet";
 
 export default function App() {
   const [text, setText] = useState("");
+  const [visible, setVisible] = useState(false);
 
   useEffect(() => {
     if (text.length === 28) {
@@ -11,20 +12,34 @@ export default function App() {
     }
   }, [text]);
 
+  // Frase
   changeTxt = (text) => {
     setText(`Seja bem-vindo, ${text}.`);
   };
 
+  visModal = (vis) => {
+    if (!visible) {
+      setVisible(vis);
+    } else {
+      setVisible(vis);
+    }
+  };
+
   return (
     <View style={styles.container}>
+      {/* Caixa de texto para colocar o nome, com limitação de caracteres. */}
       <TextInput
-        placeholder="Digite seu nome"
+        placeholder="Type your name"
         style={styles.input}
         onChangeText={changeTxt}
         maxLength={15}
       />
 
       <Text>{text}</Text>
+
+      <Button title="Open modal" onPress={() => visModal(true)} />
+      {/* true or false*/}
+      <Modal transparent={true} animationType="fade" visible={visible}></Modal>
     </View>
 
     // const [count, setCount] = useState(0);
