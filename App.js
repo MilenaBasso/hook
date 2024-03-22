acb281b138d02761f53c628db9d7d189485b9e62
Milena <milena.basso444@gmail.com>
2024-03-22 10:59:06 -0300

    START

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button, TextInput, Modal } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState();
+
+  incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  decrementCount = () => {
+    setCount(count - 1);
+  };
+
+  changeTxt = (name) => {
+    setName(`Olá ${name}, bom dia. Tudo bom?`);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Contador: {count}</Text>
+      <Button title="Increment" onPress={incrementCount} />
+      <Button title="Decrement" onPress={decrementCount} />
+      <TextInput
+        placeholder="Digite seu nome"
+        style={{
+          width: 300,
+          padding: 10,
+          borderWidth: 1,
+          borderColor: "black",
+        }}
+        onChangeText={changeTxt}
+      />
+      <Text>{name}</Text>
     </View>
   );
 }
@@ -13,8 +40,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
